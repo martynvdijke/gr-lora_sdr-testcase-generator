b8513e68bbb50068043a101c063afdb694df13ee
Martyn <martijnvdijke600@gmail.com>
2021-04-07 09:40:38 +0200

    update on test cases limit number of frames to 1

--- template/testcase_tx_rx.py
+++ template/testcase_tx_rx.py
@@ -58,7 +58,7 @@ class qa_tx_rx(gr_unittest.TestCase):
         self.sf = sf = @@sf@@
         self.samp_rate = samp_rate = bw
         self.pay_len = pay_len = @@pay_len@@
-        self.n_frame = n_frame = 10
+        self.n_frame = n_frame = 1
         self.impl_head = impl_head = @@impl_head@@
         self.has_crc = has_crc = @@has_crc@@
         self.frame_period = frame_period = 200
